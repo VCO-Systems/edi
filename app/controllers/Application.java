bee0c4a67bb37cb74bee7ff1ee9fbeeabe4b85b5
nuevia <vcahall@gmail.com>
2013-12-09 15:12:05 -0500

    added basic scaffolding for db import feature (button, server returns list of tables in 'customer_db_1')

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,7 +5,18 @@ import javax.persistence.*;
 import play.*;
 import play.mvc.*;
 import play.libs.Json;
+
+import org.codehaus.jackson.node.ArrayNode;
 import org.codehaus.jackson.node.ObjectNode;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import com.avaje.ebean.Ebean;
 
@@ -33,7 +44,7 @@ public class Application extends Controller {
     		List<Mapping> matches = Mapping.find.where()
     				.eq("id", mappingNodeId)
     				.findList();
-    		Logger.debug(String.valueOf(matches.size()));
+    		//Logger.debug(String.valueOf(matches.size()));
     	    // If we found a matching mappind document, return it
     		if (matches.size() > 0) {
     	    	result.put("data", Json.toJson(matches));
@@ -43,7 +54,7 @@ public class Application extends Controller {
     		}
     	}
     	else if (mappingNodeId == null) {
-    		Logger.debug("Returning all mappings");
+    		//Logger.debug("Returning all mappings");
     	}
     	return ok(result);
     }
@@ -60,6 +71,63 @@ public class Application extends Controller {
     		result.put("status", 0);
     		result.put("message", "Deletion failed.  Record not found.");
     	}
+    	return ok(result);
+    }
+    
+    public static Result getSchema(String database_name) {
+    	ObjectNode result = Json.newObject();
+    	//List<Object> data = new ArrayList<Object>();
+    	ArrayNode data = result.putArray("data");
+
+    	Connection con = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:postgresql://localhost/customer_db_1";
+        String user = "postgres";
+        String password = "postgres";
+ 
+        try {
+
+            con = DriverManager.getConnection(url, user, password);
+            String query = "SELECT table_name FROM information_schema.tables "
+                    + "WHERE table_schema = 'public'";
+            pst = con.prepareStatement(query);
+
+            rs = pst.executeQuery();
+
+            while (rs.next()) {
+                //System.out.println(rs.getString(1));
+            	ObjectNode tbl = Json.newObject(); 
+            	tbl.put("table_name", rs.getString(1));
+            	data.add(tbl);
+            }
+
+        } catch (SQLException ex) {
+//            Logger lgr = Logger.getLogger(ListTables.class.getName());
+//            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        	  System.out.println(ex.getMessage());
+
+        } finally {
+
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pst != null) {
+                    pst.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                
+                //Logger lgr = Logger.getLogger(ListTables.class.getName());
+                //lgr.log(Level.WARNING, ex.getMessage(), ex);
+            	System.out.println(ex.getMessage());
+            }
+        }
     	return ok(result);
     }
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,66 +1,67 @@
-angular.module('ediApp', ['ngRoute','ngGrid']).
-   config(function ($routeProvider) {
-    $routeProvider.
-       when('/', {
-           controller: 'HomeCtrl',
-           templateUrl: 'assets/partials/home.html'
-       }).
-       when('/database', {
-           controller: 'DatabaseEditorCtrl',
-           templateUrl: 'assets/partials/database-editor.html'
-       }).
-       otherwise({
-           redirectTo: '/'
-       });
+angular.module('ediApp', ['ngRoute', 'ngGrid', '$strap.directives']).config(function($routeProvider) {
+   $routeProvider.when('/', {
+      controller : 'HomeCtrl',
+      templateUrl : 'assets/partials/home.html'
+   }).when('/database', {
+      controller : 'DatabaseEditorCtrl',
+      templateUrl : 'assets/partials/database-editor.html'
+   }).otherwise({
+      redirectTo : '/'
+   });
 })
-
 /**
  * The home screen provides create/load/delete functionality
  * for the mapping documents.
- *  
- */
-.controller("HomeCtrl", ['$scope', 'mappingService', function($scope, mappingService) {
+ *
+ */.controller("HomeCtrl", ['$scope', 'mappingService',
+function($scope, mappingService) {
    $scope.pageTitle = "Mapping Definitions";
    $scope.tableData = mappingService.tableData;
-   
+
    // Initialize the grid that displays the mapping documents
    $scope.mappingDocuments = mappingService.mappingDocuments;
    $scope.selectedMappingDocuments = [];
-   $scope.gridOptions = { 
-      data: 'mappingDocuments',
-      columnDefs: [
-         {field:'name', displayName:'Name'}, 
-         {field:'create_ts', displayName:'Created On', width: 125},
-         {field:'mod_ts', displayName:'Last Edited On', width: 125},
-         {field:'create_user', displayName:'Created By', width: 175}
-      ],
-      multiSelect: false,
-      enableColumnResize: true,
-      selectedItems: $scope.selectedMappingDocuments
+   $scope.gridOptions = {
+      data : 'mappingDocuments',
+      columnDefs : [{
+         field : 'name',
+         displayName : 'Name'
+      }, {
+         field : 'create_ts',
+         displayName : 'Created On',
+         width : 125
+      }, {
+         field : 'mod_ts',
+         displayName : 'Last Edited On',
+         width : 125
+      }, {
+         field : 'create_user',
+         displayName : 'Created By',
+         width : 175
+      }],
+      multiSelect : false,
+      enableColumnResize : true,
+      selectedItems : $scope.selectedMappingDocuments
    };
-   
-   
-   
+
    /**
-    * Load the list of mappings to be displayed in the grid. 
+    * Load the list of mappings to be displayed in the grid.
     */
-   
+
    $scope.loadMappings = function() {
-      mappingService.getMappings()
-         .success(function (dt) {
-             mappingService.mappingDocuments=dt.data;
-             $scope.mappingDocuments = mappingService.mappingDocuments;
-             // A bug in ng-grid causes cols with width:auto to only resize
-             // on domReady.  So we need to force the grid to redraw now that it has data
-             
-         })
-         .error(function (error) {
-             $scope.status = 'Unable to load mappingDocuments: ' + error.message;
-         });
+      mappingService.getMappings().success(function(dt) {
+         mappingService.mappingDocuments = dt.data;
+         $scope.mappingDocuments = mappingService.mappingDocuments;
+         // A bug in ng-grid causes cols with width:auto to only resize
+         // on domReady.  So we need to force the grid to redraw now that it has data
+
+      }).error(function(error) {
+         $scope.status = 'Unable to load mappingDocuments: ' + error.message;
+      });
    };
-   
+
    /**
-    * Load all the data for a mapping. 
+    * Load all the data for a mapping.
     */
    $scope.loadMapping = function() {
       if ($scope.selectedMappingDocuments.length > 0) {
@@ -68,90 +69,149 @@ angular.module('ediApp', ['ngRoute','ngGrid']).
          mappingService.loadMappingData(selectedMappingId);
       }
    };
-   
+
    $scope.deleteMapping = function() {
-       if ($scope.selectedMappingDocuments.length > 0) {
+      if ($scope.selectedMappingDocuments.length > 0) {
          var selectedMappingId = $scope.selectedMappingDocuments[0].id;
-         mappingService.deleteMapping(selectedMappingId)
-            .success(function (dt) {
-                if (dt.status == 1) {
-                   // Record was deleted.  Reload mappings.
-                   $scope.loadMappings();
-                }
-            })
-            .error(function (error) {
-                $scope.status = 'Unable to delete mapping document: ' + error.message;
-            });
+         mappingService.deleteMapping(selectedMappingId).success(function(dt) {
+            if (dt.status == 1) {
+               // Record was deleted.  Reload mappings.
+               $scope.loadMappings();
+            }
+         }).error(function(error) {
+            $scope.status = 'Unable to delete mapping document: ' + error.message;
+         });
       }
    };
-   
+
    init();
-   
+
    function init() {
       //  Request mapping documents from the server, if they've
       //  not yet been loaded.
       if (!mappingService.mappingDocuments) {
-          $scope.loadMappings();
-       }   
-      
+         $scope.loadMappings();
+      }
+
       // Listen for the grid's dataprovider to change
-      $scope.$on('ngGridEventData', function(e,gridId) {
+      $scope.$on('ngGridEventData', function(e, gridId) {
          //$scope.gridOptions
       });
    };
 }])
 
-.controller("DatabaseEditorCtrl", ['$scope', 'mappingService', function($scope, mappingService) {
+/**
+ * Controller for the "Database Editor" screen.
+ */.controller("DatabaseEditorCtrl", ['$scope', 'mappingService',
+function($scope, mappingService) {
    //$scope.list = mappingService.list;
+   
+   // Set up the "import db schema" wizard
+   $scope.steps = ['one', 'two', 'three'];
+   $scope.step = 0;
+   $scope.import_data = {};
+
+   $scope.isCurrentStep = function(step) {
+      return $scope.step === step;
+   };
+
+   $scope.setCurrentStep = function(step) {
+      $scope.step = step;
+   };
+
+   $scope.getCurrentStep = function() {
+      return $scope.steps[$scope.step];
+   };
+
+   $scope.isFirstStep = function() {
+      return $scope.step === 0;
+   };
+
+   $scope.isLastStep = function() {
+      return $scope.step === ($scope.steps.length - 1);
+   };
+
+   $scope.getNextLabel = function() {
+      return ($scope.isLastStep()) ? 'Submit' : 'Next';
+   };
+
+   $scope.handlePrevious = function() {
+      $scope.step -= ($scope.isFirstStep()) ? 0 : 1;
+   };
+
+   $scope.handleNext = function(dismiss) {
+      if ($scope.isLastStep()) {
+         dismiss();
+      } else {
+         $scope.step += 1;
+      }
+   };
+   
+   $scope.test = function() {
+       
+   };
+   
+   $scope.importSchema = function() {
+      mappingService.importDatabaseSchema("fakeUrl").success(function(dt) {
+         //mappingService.mappingDocuments = dt.data;
+         alert(dt.data);
+
+      }).error(function(error) {
+         $scope.status = 'Unable to import db schema: ' + error.message;
+      }); 
+   };
+
    $scope.title = mappingService.title;
    $scope.tm = tableManager();
    $scope.tableData;
-   
+
    init();
 
    function init() {
-       if (!mappingService.tableData) {
-          getSampleData();
-       }   
+      if (!mappingService.tableData) {
+         getSampleData();
+      }
+   };
+
+   function getSampleData() {
+      mappingService.getMappings().success(function(dt) {
+         mappingService.tableData = dt.data;
+         $scope.tableData = mappingService.tableData;
+         console.debug($scope.tableData);
+      }).error(function(error) {
+         $scope.status = 'Unable to load sample table data: ' + error.message;
+      });
+   }
+
+}]).factory('mappingService', ['$http',
+function($http) {
+   var urlGetMappings = 'mappings/get';
+   // 'assets/data/sample-data.json';
+   var urlMapping = 'load_mapping?id=';
+   var urlDeleteMapping = 'delete_mapping?id=';
+   var mappingService = {};
+
+   mappingService.tableData;
+   mappingService.mappingDocuments;
+
+   // Request a list of all mapping documents
+   mappingService.getMappings = function() {
+      return $http.get(urlGetMappings);
+   };
+
+   // Load all the data for a mapping document
+   mappingService.loadMappingData = function(mappingNodeId) {
+      return $http.get(urlMapping + mappingNodeId);
+   };
+
+   mappingService.deleteMapping = function(mappingId) {
+      return $http.get(urlDeleteMapping + mappingId);
    };
    
-    function getSampleData() {
-        mappingService.getMappings()
-            .success(function (dt) {
-                mappingService.tableData=dt.data;
-                $scope.tableData = mappingService.tableData;
-                console.debug($scope.tableData);
-            })
-            .error(function (error) {
-                $scope.status = 'Unable to load sample table data: ' + error.message;
-            });
-    }
-}])
+   // Load all the data for a mapping document
+   mappingService.importDatabaseSchema = function(db_url) {
+      return $http.get("schema/get?database_name=" + db_url);
+   };
 
-.factory('mappingService', ['$http', function($http) {
-    var urlGetMappings = 'mappings/get';  // 'assets/data/sample-data.json';
-    var urlMapping = 'load_mapping?id=';
-    var urlDeleteMapping = 'delete_mapping?id=';
-    var mappingService = {};
-    
-    mappingService.tableData;
-    mappingService.mappingDocuments;
-    
-    // Request a list of all mapping documents
-    mappingService.getMappings = function () {
-        return $http.get(urlGetMappings);
-    };
-    
-    // Load all the data for a mapping document
-    mappingService.loadMappingData = function (mappingNodeId) {
-        return $http.get(urlMapping + mappingNodeId);
-    };
-    
-    mappingService.deleteMapping = function(mappingId) {
-        return $http.get(urlDeleteMapping + mappingId);
-    };
-
-    
-
-    return mappingService;
+   return mappingService;
 }]);
